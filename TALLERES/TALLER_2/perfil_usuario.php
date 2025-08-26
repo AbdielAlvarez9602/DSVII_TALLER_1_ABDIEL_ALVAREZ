e2c4f6875ed67213e16b9be992239400c0546b2d
abdielalvarez962 <abdielalvarez962@gmail.com>
2025-08-25 20:20:07 -0500

    Ejercicio de perfil de usuario completado

---
+++ TALLERES/TALLER_2/perfil_usuario.php
@@ -0,0 +1,17 @@
+<?php
+// Variables
+$nombre_completo = "Abdiel Alvarez";
+$edad = 25;
+$correo = "abdielalvarez9602@example.com";
+$telefono = "6524-2568";
+
+// Constante
+define("OCUPACION", "Estudiante");
+
+// Mostrar información con echo
+echo "Nombre completo: " . $nombre_completo . "<br>";
+echo "Edad: " . $edad . "<br>";
+echo "Correo: " . $correo . "<br>";
+echo "Teléfono: " . $telefono . "<br>";
+echo "Ocupación: " . OCUPACION . "<br>";
+?>
